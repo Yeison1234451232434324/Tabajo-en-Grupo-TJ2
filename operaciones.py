889bca002c79b6fbd73d05e2d18b679b97435bb4
juansebastianmorales <moralesmoralessebastian50@gmail.com>
2026-02-16 19:06:57 -0500

    Combinar código de main y rama2

--- operaciones.py
+++ operaciones.py
@@ -1,3 +1,22 @@
+# version 1.0 yeison fabian
+num1 = float(input("Escribe el primer número: "))
+num2 = float(input("Escribe el segundo número: "))
+
+
+multiplicacion = num1 * num2
+
+
+print("-" * 20)
+print(f"El resultado de multiplicar {num1} por {num2} es: {multiplicacion}")
+
+
+if num2 != 0:
+    division = num1 / num2
+    print(f"El resultado de dividir {num1} entre {num2} es: {division}")
+else:
+    print("No puedo dividir entre cero, ¡el universo colapsaría!")
+print("-" * 20)
+
 # version 2.0 Angel Gutierrez
 num1 = float(input("Escribe el primer número: "))
 num2 = float(input("Escribe el segundo número: "))
