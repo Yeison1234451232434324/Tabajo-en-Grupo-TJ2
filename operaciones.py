162a9746e400daf02802e95d7fe0ec08a57403f0
Yeison1234451232434324 <yeisonvargas8022@gmail.com>
2026-02-17 08:03:37 -0500

    division_ramas

--- operaciones.py
+++ operaciones.py
@@ -17,14 +17,3 @@ else:
     print("No puedo dividir entre cero, ¡el universo colapsaría!")
 print("-" * 20)
 
-# version 2.0 Angel Gutierrez
-num1 = float(input("Escribe el primer número: "))
-num2 = float(input("Escribe el segundo número: "))
-print("-" * 20)
-# SUMA
-suma = num1 + num2
-print(f"La suma de {num1} + {num2} es: {suma}")
-# RESTA
-resta = num1 - num2
-print(f"La resta de {num1} - {num2} es: {resta}")
-print("-" * 20)
